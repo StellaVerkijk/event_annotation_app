52f2af62afe540ce2d456cc1b4454dbe44222e4f
StellaVerkijk <stellaverkijk@hotmail.com>
2026-01-28 15:18:49 +0100

    first files for visualization in streamlit

---
+++ csv_to_json.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import json
+
+def split_at_newlines(lst):
+    '''
+    Written by Claude
+    '''
+    result = []
+    current_group = []
+    
+    for item in lst:
+        if item == "\\n":
+            if current_group:  # Only add non-empty groups
+                result.append(current_group)
+                current_group = []
+        else:
+            current_group.append(item)
+    
+    # Don't forget the last group
+    if current_group:
+        result.append(current_group)
+    
+    return result
+
+def tojson(sentences, sentence_annotations, outfile):
+    with open(outfile, 'w') as f:
+        for i in range(0, len(sentences)):
+            tokens, labels = sentences[i], sentence_annotations[i]
+            json.dump({'words': tokens, 'events': labels}, f)
+            f.write("\n")
+
+def check_length_of_longest():
+    longest = max(sentences, key=len)
+    print(len(longest))
+
+INFILE = '3604.csv'
+OUTFILE = '3604.json'
+
+df = pd.read_csv(INFILE, index_col=0, encoding='utf-8')
+print(df['word'].tolist())
+sentences = split_at_newlines(df['word'].tolist())
+sentence_annotations = split_at_newlines(df['manual_resolve'].tolist())
+
+print(len(sentences))
+print(len(sentence_annotations))
+
+tojson(sentences, sentence_annotations, OUTFILE)
+
+
+
+
+
---
+++ make_streamlit.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import json
+import ast
+
+def convert_to_annotated_text(data):
+    """
+    Written by Claude
+
+    Convert words and events data to annotated_text format.
+    
+    Args:
+        data: dict with 'words' and 'events' keys
+    
+    Returns:
+        list of tuples and strings for annotated_text
+    """
+    words = data['words']
+    events = data['events']
+    
+    result = []
+    current_text = []
+    current_event = None
+    current_event_words = []
+    
+    for word, event in zip(words, events):
+        # Check if this is a B- (Begin) tag or different event
+        if event.startswith('B-'):
+            # Save any accumulated non-event text
+            if current_text:
+                result.append(' '.join(current_text))
+                current_text = []
+            
+            # Save any previous event
+            if current_event_words and current_event:
+                result.append((' '.join(current_event_words) + ' ', current_event))
+                current_event_words = []
+            
+            # Start new event
+            current_event = event[2:]  # Remove 'B-' prefix
+            current_event_words = [word]
+            
+        elif event.startswith('I-'):
+            # Continue current event
+            current_event_words.append(word)
+            
+        else:  # event == 'O' (no event)
+            # Save any previous event first
+            if current_event_words and current_event:
+                result.append((' '.join(current_event_words) + ' ', current_event))
+                current_event_words = []
+                current_event = None
+            
+            # Add to regular text
+            current_text.append(word)
+    
+    # Don't forget remaining text/events
+    if current_text:
+        result.append(' '.join(current_text))
+    if current_event_words and current_event:
+        result.append((' '.join(current_event_words) + ' ', current_event))
+    
+    return result
+
+with open('3604.json') as f:
+    data = f.readlines()
+
+#for line in data:
+    #annotated = convert_to_annotated_text(line)
+
+print("EXAMPLE OF DATA")
+print(data[3])
+print(type(ast.literal_eval(data[3])))
+
+regions = []
+
+for line in data:   
+    regions.append(convert_to_annotated_text(ast.literal_eval(line)))
+    
+st.header("Gold data for Events")
+from annotated_text import annotated_text
+for r in regions:
+    annotated_text(r)
